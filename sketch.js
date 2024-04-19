b94052c06d95970fef1324ad9c6b3faa886426ae
Artur Augusto <166838162+ArturAugusto0@users.noreply.github.com>
2024-04-19 12:10:14 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(1000, 1000);
+    background('black');
+  }
+  
+  function draw() {
+    stroke('red');
+    fill('red');
+    
+    if(mouseIsPressed){
+      rect(mouseX, mouseY, 10, 35)
+      
+      
+      
+    }
+  }
